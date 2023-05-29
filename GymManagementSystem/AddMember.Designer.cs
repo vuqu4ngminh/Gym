e27e7712b654391c99021ac061724dafd11c2287
H1epVu <115410774+H1epVu@users.noreply.github.com>
2023-05-29 18:41:09 +0700

    first commit

---
+++ GymManagementSystem/AddMember.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace GymManagementSystem
+{
+    partial class AddMember
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameTb = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PhoneTb = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AgeTb = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.GenderCb = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AmountTb = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(425, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(514, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ADD NEW MEMBER";
+            // 
+            // NameTb
+            // 
+            this.NameTb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.NameTb.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.NameTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.NameTb.HintForeColor = System.Drawing.Color.Empty;
+            this.NameTb.HintText = "";
+            this.NameTb.isPassword = false;
+            this.NameTb.LineFocusedColor = System.Drawing.Color.Black;
+            this.NameTb.LineIdleColor = System.Drawing.Color.Gray;
+            this.NameTb.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.NameTb.LineThickness = 3;
+            this.NameTb.Location = new System.Drawing.Point(52, 230);
+            this.NameTb.Margin = new System.Windows.Forms.Padding(4);
+            this.NameTb.Name = "NameTb";
+            this.NameTb.Size = new System.Drawing.Size(294, 44);
+            this.NameTb.TabIndex = 2;
+            this.NameTb.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Member Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(533, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Monthly Amount";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // PhoneTb
+            // 
+            this.PhoneTb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PhoneTb.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.PhoneTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.PhoneTb.HintForeColor = System.Drawing.Color.Empty;
+            this.PhoneTb.HintText = "";
+            this.PhoneTb.isPassword = false;
+            this.PhoneTb.LineFocusedColor = System.Drawing.Color.Black;
+            this.PhoneTb.LineIdleColor = System.Drawing.Color.Gray;
+            this.PhoneTb.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.PhoneTb.LineThickness = 3;
+            this.PhoneTb.Location = new System.Drawing.Point(538, 230);
+            this.PhoneTb.Margin = new System.Windows.Forms.Padding(4);
+            this.PhoneTb.Name = "PhoneTb";
+            this.PhoneTb.Size = new System.Drawing.Size(294, 44);
+            this.PhoneTb.TabIndex = 5;
+            this.PhoneTb.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1025, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 28);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Age";
+            // 
+            // AgeTb
+            // 
+            this.AgeTb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.AgeTb.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.AgeTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.AgeTb.HintForeColor = System.Drawing.Color.Empty;
+            this.AgeTb.HintText = "";
+            this.AgeTb.isPassword = false;
+            this.AgeTb.LineFocusedColor = System.Drawing.Color.Black;
+            this.AgeTb.LineIdleColor = System.Drawing.Color.Gray;
+            this.AgeTb.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.AgeTb.LineThickness = 3;
+            this.AgeTb.Location = new System.Drawing.Point(1030, 230);
+            this.AgeTb.Margin = new System.Windows.Forms.Padding(4);
+            this.AgeTb.Name = "AgeTb";
+            this.AgeTb.Size = new System.Drawing.Size(294, 44);
+            this.AgeTb.TabIndex = 7;
+            this.AgeTb.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(47, 374);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 28);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Gender";
+            // 
+            // GenderCb
+            // 
+            this.GenderCb.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenderCb.FormattingEnabled = true;
+            this.GenderCb.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.GenderCb.Location = new System.Drawing.Point(52, 442);
+            this.GenderCb.Name = "GenderCb";
+            this.GenderCb.Size = new System.Drawing.Size(294, 36);
+            this.GenderCb.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(533, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(185, 28);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Phone Number";
+            // 
+            // AmountTb
+            // 
+            this.AmountTb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.AmountTb.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.AmountTb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.AmountTb.HintForeColor = System.Drawing.Color.Empty;
+            this.AmountTb.HintText = "";
+            this.AmountTb.isPassword = false;
+            this.AmountTb.LineFocusedColor = System.Drawing.Color.Black;
+            this.AmountTb.LineIdleColor = System.Drawing.Color.Gray;
+            this.AmountTb.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.AmountTb.LineThickness = 3;
+            this.AmountTb.Location = new System.Drawing.Point(538, 442);
+            this.AmountTb.Margin = new System.Windows.Forms.Padding(4);
+            this.AmountTb.Name = "AmountTb";
+            this.AmountTb.Size = new System.Drawing.Size(294, 44);
+            this.AmountTb.TabIndex = 11;
+            this.AmountTb.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(435, 602);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 41);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "ADD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(613, 602);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 41);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(793, 602);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 41);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "BACK";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(590, 677);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(151, 41);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "RETURN";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // AddMember
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1369, 792);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AmountTb);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.GenderCb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.AgeTb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PhoneTb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameTb);
+            this.Controls.Add(this.label1);
+            this.Name = "AddMember";
+            this.Text = "AddMember";
+            this.Load += new System.EventHandler(this.AddMember_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox NameTb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox PhoneTb;
+        private System.Windows.Forms.Label label4;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox AgeTb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox GenderCb;
+        private System.Windows.Forms.Label label6;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox AmountTb;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
