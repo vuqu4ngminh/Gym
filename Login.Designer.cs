d030563577fe4a6de50598a19289a96bc1b24b10
vuqu4ngminh <solution.vuqu4ngminh@gmail.com>
2023-06-19 00:31:13 +0700

    Added MD5 Hash and ADO.NET connection

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -32,68 +32,89 @@
             this.UidTb = new System.Windows.Forms.TextBox();
             this.PassTb = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(231, 35);
+            this.label1.Font = new System.Drawing.Font("Verdana", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(345, 53);
+            this.label1.Size = new System.Drawing.Size(438, 45);
             this.label1.TabIndex = 0;
-            this.label1.Text = "GYM CENTER";
+            this.label1.Text = "Quản Lý Phòng Gym";
             // 
             // UidTb
             // 
-            this.UidTb.Location = new System.Drawing.Point(268, 162);
+            this.UidTb.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UidTb.Location = new System.Drawing.Point(210, 132);
+            this.UidTb.Margin = new System.Windows.Forms.Padding(2);
             this.UidTb.Name = "UidTb";
-            this.UidTb.Size = new System.Drawing.Size(268, 22);
+            this.UidTb.Size = new System.Drawing.Size(241, 27);
             this.UidTb.TabIndex = 1;
             // 
             // PassTb
             // 
-            this.PassTb.Location = new System.Drawing.Point(268, 208);
+            this.PassTb.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassTb.Location = new System.Drawing.Point(210, 180);
+            this.PassTb.Margin = new System.Windows.Forms.Padding(2);
             this.PassTb.Name = "PassTb";
             this.PassTb.PasswordChar = '*';
-            this.PassTb.Size = new System.Drawing.Size(268, 22);
+            this.PassTb.Size = new System.Drawing.Size(241, 27);
             this.PassTb.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(343, 274);
+            this.button1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(229, 230);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 41);
+            this.button1.Size = new System.Drawing.Size(127, 33);
             this.button1.TabIndex = 3;
-            this.button1.Text = "LOGIN";
+            this.button1.Text = "Đăng Nhập";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // label2
             // 
-            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(343, 335);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 41);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "RESET";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(105, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tài Khoản";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(105, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mật Khẩu";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.PassTb);
             this.Controls.Add(this.UidTb);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Login";
-            this.Text = "Form1";
+            this.Text = "Gym";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,7 +126,8 @@
         private System.Windows.Forms.TextBox UidTb;
         private System.Windows.Forms.TextBox PassTb;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Login.cs
+++ Login.cs
@@ -2,43 +2,94 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace GymManagementSystem
 {
+    public class MD5Hashing
+    {
+        public static string GetMD5Hash(string input)
+        {
+            // Tạo một đối tượng MD5
+            using (MD5 md5 = MD5.Create())
+            {
+                // Chuyển đổi chuỗi đầu vào sang mảng byte
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+                // Mã hóa mảng byte
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Chuyển đổi mảng byte thành chuỗi hexa
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+
+                // Trả về chuỗi đã mã hóa
+                return sb.ToString();
+            }
+        }
+    }
+
     public partial class Login : Form
     {
+        private const string connectionString = "Data Source=DEVICE;Initial Catalog=GYM;Integrated Security=True";
         public Login()
         {
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private bool checkAdmin(string _username, string _password)
         {
-            UidTb.Text = "";
-            PassTb.Text = "";
+            string query = "SELECT * FROM tbl_admin";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connectionString);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            foreach (DataRow row in table.Rows)
+            {
+                // Truy cập các cột và kiểm tra giá trị
+                string username = (string)row["username"];
+                string password = (string)row["password"];
+                if (_username == username && _password == password)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(UidTb.Text == "" || PassTb.Text == "")
+            string currentPwd = MD5Hashing.GetMD5Hash(PassTb.Text);
+
+            if (UidTb.Text == "" || PassTb.Text == "")
             {
-                MessageBox.Show("Missing Information");
+                MessageBox.Show("Thông tin bị trống");
             }
-            else if (UidTb.Text == "Admin" && PassTb.Text == "Admin")
+            else if (checkAdmin(UidTb.Text, currentPwd))
             {
+
                 MainForm mainform = new MainForm();
                 mainform.Show();
                 this.Hide();
             }
             else
             {
-                MessageBox.Show("Wrong Id or Password");
+                MessageBox.Show("Sai tên tài khoản hoặc mật khẩu");
             }
         }
+
     }
 }
